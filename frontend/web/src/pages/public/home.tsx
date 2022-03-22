623b857ec9a4ca0ae1f0a227a9ac054c04d1aca2
Justin Rahardjo <justindra91@gmail.com>
2022-03-21 21:48:10 -0700

    chore: remove auth0 duplicate

--- frontend/web/src/pages/public/home.tsx
+++ frontend/web/src/pages/public/home.tsx
@@ -1,30 +1,3 @@
-import { useAuth0 } from '@auth0/auth0-react';
-
-export function AuthStatus() {
-  let { isAuthenticated, user, logout } = useAuth0();
-
-  if (!isAuthenticated) {
-    return <p>You are not logged in.</p>;
-  }
-
-  return (
-    <p>
-      Welcome {user?.name}!{' '}
-      <button
-        onClick={() => {
-          logout({ returnTo: window.location.origin });
-        }}>
-        Sign out
-      </button>
-    </p>
-  );
-}
-
 export const HomePage: React.FC = () => {
-  return (
-    <div>
-      This is the home page.
-      <AuthStatus />
-    </div>
-  );
+  return <div>This is the home page.</div>;
 };
